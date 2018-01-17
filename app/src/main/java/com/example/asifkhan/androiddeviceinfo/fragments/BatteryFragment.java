51af25dbaa088bc7e739ab9c49098c206a1e7eb2
Asif Khan <asifkhan.myapps@gmail.com>
2018-01-18 04:45:56 +0600

    new fragments added

---
+++ app/src/main/java/com/example/asifkhan/androiddeviceinfo/fragments/BatteryFragment.java
@@ -0,0 +1,21 @@
+package com.example.asifkhan.androiddeviceinfo.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.asifkhan.androiddeviceinfo.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BatteryFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.fragment_battery, container, false);
+        return view;
+    }
+}
