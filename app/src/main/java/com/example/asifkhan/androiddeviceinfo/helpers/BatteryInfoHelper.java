6a1e52a571dccca8833aae564d33e00e5a883e3f
Asif Khan <asifkhan.myapps@gmail.com>
2018-01-24 03:02:24 +0600

    new class created for some constants containing battery info

---
+++ app/src/main/java/com/example/asifkhan/androiddeviceinfo/helpers/BatteryInfoHelper.java
@@ -0,0 +1,14 @@
+package com.example.asifkhan.androiddeviceinfo.helpers;
+
+/**
+ * Created by asifkhan on 1/19/18.
+ */
+
+public class BatteryInfoHelper {
+    public static final String BATTERY_LEVEL_TEXT="Battery Level";
+    public static final String BATTERY_TYPE_TEXT="Battery Type";
+    public static final String POWER_SOURCE_TEXT="Power Connector";
+    public static final String BATTERY_TEMPERATURE_TEXT="Battery Temperature";
+    public static final String BATTERY_VOLTAGE_TEXT="Voltage";
+    public static final String BATTERY_STATUS="Status";
+}
